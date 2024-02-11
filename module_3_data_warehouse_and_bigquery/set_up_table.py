0e4e47fc128087a650377e3a284e0c1768392592
nguyennhu0805 <104962044+nguyennhu0805@users.noreply.github.com>
2024-02-11 22:26:03 +0700

    Update set_up_table.py

--- module_3_data_warehouse_and_bigquery/set_up_table.py
+++ module_3_data_warehouse_and_bigquery/set_up_table.py
@@ -1,1 +1,10 @@
+CREATE OR REPLACE EXTERNAL TABLE
+  `theta-byte-412315.nyc_taxi.green_taxi_external`
+  OPTIONS(
+    format ="parquet",
+    uris = ['gs://mage-zoomcamp-matt-palmer-nn/green_taxi/lpep_pickup_date=2022-*']
+    );
 
+create or replace table `theta-byte-412315.nyc_taxi.green_taxi` as
+SELECT * except (__index_level_0__)
+FROM `theta-byte-412315.nyc_taxi.green_taxi_external`
