d80e7f01d021d37b180e4231e9459efa40e70c26
quynhnhu.nguyenthi.0805@gmail.com <quynhnhu.nguyenthi.0805@gmail.com>
2024-02-28 10:10:56 +0000

    commit

--- models/stag/stag_green_taxi.sql
+++ models/stag/stag_green_taxi.sql
@@ -1,5 +1,10 @@
 {{config(miterialized = 'view')}}
 
-SELECT *, {{the_macro_test_name ('total_amount')}} AS test_macro
+SELECT *
+, {{the_macro_test_name ('total_amount')}} AS test_macro
+, {{dbt_utils.surrogate_key(['vendor_id', 'lpep_pickup_datetime'])}} AS trip_id
 FROM {{source ('staging', 'green_taxi')}}
-LIMIT 100
+
+{%if var('is_test_run', default = true)%}
+LIMIT 10
+{%endif%}
